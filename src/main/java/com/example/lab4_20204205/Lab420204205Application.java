806e4bc0159f888cba3459d4094bad960d054925
ola <ola@ola.com>
2025-05-09 20:27:51 -0500

    primer commit

---
+++ src/main/java/com/example/lab4_20204205/Lab420204205Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20204205;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420204205Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420204205Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab4_20204205/Lab420204205ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20204205;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420204205ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
